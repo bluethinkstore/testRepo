61841d9cca69cd897453c3668f99d7783aa4e55f
jagriti Gupta <jagriti@dotcomweavers.com>
2023-03-29 17:27:00 +0530

    custom nevigater of my account

---
+++ app/code/MageDelight/Tutorial/Controller/Account/Custom.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace MageDelight\Tutorial\Controller\Account;
+
+use Magento\Framework\View\Result\PageFactory;
+
+use Magento\Framework\App\Action\Context;
+
+class Custom extends \Magento\Customer\Controller\AbstractAccount
+
+{
+
+    /**
+
+     * @var PageFactory
+
+     */
+
+    protected $resultPageFactory;
+
+    /**
+
+     * @param Context                                             $context
+
+     * @param PageFactory                                         $resultPageFactory
+
+     * @param \Magento\Framework\Controller\Result\ForwardFactory $resultForwardFactory
+
+     * @param \MageDelight\CustomerAttachments\Helper\Data            $dataHelper
+
+     */
+
+    public function __construct(
+
+        Context $context,
+
+        PageFactory $resultPageFactory
+
+    ) {
+
+        $this->resultPageFactory    = $resultPageFactory;
+
+        parent::__construct($context);
+
+    }
+
+    /**
+
+     * @return \Magento\Framework\View\Result\Page
+
+     */
+
+    public function execute()
+
+    {
+
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+
+        $resultPage = $this->resultPageFactory->create();
+
+        $resultPage->getConfig()->getTitle()->set(__('Myyyy Custom'));
+
+        return $resultPage;
+
+    }
+
+}
---
+++ app/code/MageDelight/Tutorial/registration.php
@@ -0,0 +1,4 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MageDelight_Tutorial', __DIR__);
